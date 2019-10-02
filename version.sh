9d41c593fb64209ed05f7d4b7ad272ab3705f822
C. Warren Dale <dale@cwarrendale.com>
2019-10-02 15:45:02 -0500

    Use YYYY-MM-DD.SHA for projects with no explicit versioning

--- version.sh
+++ version.sh
@@ -2,9 +2,14 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 SHA=$(git rev-parse --short HEAD)
+DATE=$(date --rfc-3339=date)
 
 if [[ $BRANCH = "master" ]]; then
-  echo $(cat .version)
+  if [[ -e ".version" ]]; then
+    echo $(cat .version)
+  else 
+    echo "${DATE}.${SHA}"
+  fi
 else
   echo "${BRANCH}/${SHA}"
 fi
