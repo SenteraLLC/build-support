228e1a6ffa0649aa1262e82fe62da939045370b4
Scott Olson <scott.olson@sentera.com>
2020-05-04 14:40:32 -0500

    dev isn't working on master

--- env-tags.sh
+++ env-tags.sh
@@ -5,6 +5,7 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [[ $BRANCH = "master" ]]; then
   echo "prod"
   echo "dev"
+  echo ""
 elif [[ $BRANCH = staging.* ]]; then
   echo "staging"
   echo "staging2"
