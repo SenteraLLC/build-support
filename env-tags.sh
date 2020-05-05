1d04280587ce92472b28aa735ccc633ac037e76c
Scott Olson <scott.olson@sentera.com>
2020-05-04 21:50:59 -0500

    spaces

--- env-tags.sh
+++ env-tags.sh
@@ -3,7 +3,7 @@
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ $BRANCH = "master" ]]; then
-  echo "prod,dev"
+  echo "prod dev"
 elif [[ $BRANCH = staging.* ]]; then
-  echo "staging,staging2"
+  echo "staging staging2"
 fi
