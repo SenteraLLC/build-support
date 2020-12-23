cba4d70b41ba79b8897d3ef18efb74cc0aa8574a
Jacob Dalton <jacob.dalton@sentera.com>
2020-12-23 08:55:43 -0600

    use prod and dev on "main" branch as well

--- env-tags.sh
+++ env-tags.sh
@@ -2,7 +2,7 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [[ $BRANCH = "master" ]]; then
+if [[ $BRANCH = "master" ]] || [[ $BRANCH = "main" ]]; then
   echo "prod dev"
 elif [[ $BRANCH = staging.* ]]; then
   echo "staging staging2"
